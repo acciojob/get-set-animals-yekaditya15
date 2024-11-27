cd283381e9348f761a04d45f9b5e46f544f57bae
Yekaditya Devadi <141273781+yekaditya15@users.noreply.github.com>
2024-11-27 20:02:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-//complete this code
-class Animal {}
+class Animal {
+    constructor(species) {
+        this._species = species;
+    }
 
-class Dog extends Animal {}
+    get species() {
+        return this._species;
+    }
 
-class Cat extends Animal {}
+    makeSound() {
+        console.log(`The ${this.species} makes a sound`);
+    }
+}
+
+class Cat extends Animal {
+    purr() {
+        console.log("purr");
+    }
+}
+
+class Dog extends Animal {
+    bark() {
+        console.log("woof");
+    }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
